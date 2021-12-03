929b9c67fc889688dddc0899c0f20e1047ee69d3
GuoJiaqi66 <858064356@qq.com>
2021-12-03 15:23:14 +0800

    2-2 后端解决bug

--- src/main/java/com/zw/service/EbookService.java
+++ src/main/java/com/zw/service/EbookService.java
@@ -13,11 +13,13 @@ import com.zw.util.CopyUtil;
 import com.zw.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@Service
 public class EbookService {
     private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
